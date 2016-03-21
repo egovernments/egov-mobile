541eec956a2005c31cebb51b40515a4ad56b0f34
Dinesh.S <dinesh.s@egovernments.org>
2016-03-21 14:58:23 +0530

    MEGOV-72 | Password level configurable from app config file

---
+++ android/src/org/egov/android/common/PasswordLevel.java
@@ -0,0 +1,9 @@
+package org.egov.android.common;
+
+public final class PasswordLevel {
+
+	public static String LOW="LOW";
+	public static String MEDIUM="MEDIUM";
+	public static String HIGH="HIGH";
+	
+}
