44bdd3b76c6d73d57430b65312704f3134b98301
Dinesh.S <dinesh.s@egovernments.org>
2017-05-17 18:06:52 +0530

    [MEGOV-179] Download service enhanced for android 7.0

--- Citizen-App/app/src/main/java/org/egovernments/egoverp/services/DownloadService.java
+++ Citizen-App/app/src/main/java/org/egovernments/egoverp/services/DownloadService.java
@@ -7,8 +7,11 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.FileProvider;
+import android.webkit.MimeTypeMap;
 
 import org.egovernments.egoverp.R;
 import org.egovernments.egoverp.api.ApiController;
@@ -148,7 +151,7 @@ public class DownloadService extends IntentService {
         download.setProgress(100);
         sendIntent(download);*/
 
-        PendingIntent pIntent = PendingIntent.getActivity(this, 0, openFile(downloadedFile), 0);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, getFileIntent(downloadedFile), 0);
         setNotificationBuilderEndStatusWithMsg("File Downloaded in Downloads Folder");
         notificationBuilder.setContentIntent(pIntent);
         Notification notification = notificationBuilder.build();
@@ -177,54 +180,22 @@ public class DownloadService extends IntentService {
     }
 
 
-    public Intent openFile(File url) {
+    public Intent getFileIntent(File downloadedFile) {
 
-        Uri uri = Uri.fromFile(url);
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        if (url.toString().contains(".doc") || url.toString().contains(".docx")) {
-            // Word document
-            intent.setDataAndType(uri, "application/msword");
-        } else if (url.toString().contains(".pdf")) {
-            // PDF file
-            intent.setDataAndType(uri, "application/pdf");
-        } else if (url.toString().contains(".ppt") || url.toString().contains(".pptx")) {
-            // Powerpoint file
-            intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
-        } else if (url.toString().contains(".xls") || url.toString().contains(".xlsx")) {
-            // Excel file
-            intent.setDataAndType(uri, "application/vnd.ms-excel");
-        } else if (url.toString().contains(".zip") || url.toString().contains(".rar")) {
-            // WAV audio file
-            intent.setDataAndType(uri, "application/x-wav");
-        } else if (url.toString().contains(".rtf")) {
-            // RTF file
-            intent.setDataAndType(uri, "application/rtf");
-        } else if (url.toString().contains(".wav") || url.toString().contains(".mp3")) {
-            // WAV audio file
-            intent.setDataAndType(uri, "audio/x-wav");
-        } else if (url.toString().contains(".gif")) {
-            // GIF file
-            intent.setDataAndType(uri, "image/gif");
-        } else if (url.toString().contains(".jpg") || url.toString().contains(".jpeg") || url.toString().contains(".png")) {
-            // JPG file
-            intent.setDataAndType(uri, "image/jpeg");
-        } else if (url.toString().contains(".txt")) {
-            // Text file
-            intent.setDataAndType(uri, "text/plain");
-        } else if (url.toString().contains(".3gp") || url.toString().contains(".mpg") || url.toString().contains(".mpeg") || url.toString().contains(".mpe") || url.toString().contains(".mp4") || url.toString().contains(".avi")) {
-            // Video files
-            intent.setDataAndType(uri, "video/*");
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        String ext = downloadedFile.getName().substring(downloadedFile.getName().lastIndexOf(".") + 1);
+        String type = mime.getMimeTypeFromExtension(ext);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", downloadedFile);
+            intent.setDataAndType(contentUri, type);
         } else {
-            //if you want you can also define the intent type for any other file
-            //additionally use else clause below, to manage other unknown extensions
-            //in this case, Android will show all applications installed on the device
-            //so you can choose which application to use
-            intent.setDataAndType(uri, "*/*");
+            intent.setDataAndType(Uri.fromFile(downloadedFile), type);
         }
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         return intent;
+
     }
 
 }
